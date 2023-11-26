eeba086ec1626b0463dc0c09cd43f6e1aae4df81
JurajSaric <149704114+JurajSaric@users.noreply.github.com>
2023-11-26 15:59:43 +0100

    Add files via upload

---
+++ Zadaca2/zad3/Zad3_DZ2_JŠ.asm
@@ -0,0 +1,68 @@
+@0
+D = A
+@5
+M = D
+
+@0
+D = M
+@L0
+D; JLE
+
+@0
+D = M
+@5
+M = M + D
+
+(L0)
+@1
+D = M
+@L1
+D; JLE  
+
+@1
+D = M
+@5
+M = M + D
+
+(L1)		
+@2
+D = M
+@L2
+D; JLE
+   
+@2
+D = M
+@5
+M = M + D
+
+(L2)
+@3
+D = M
+@L3
+D; JLE  
+
+@3
+D = M
+@5
+M = M + D
+
+(L3)
+@4
+D = M
+@L4
+D; JLE  
+
+@4
+D = M
+@5
+M = M + D
+
+(L4)
+@5
+D = M
+@5
+M = D
+
+(END)
+@END
+0; JMP
